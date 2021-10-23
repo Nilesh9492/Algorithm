cdf28d1515d7a9d979baa4330f4a4b7bdccdfdaf
Nilesh9492 <nilesh94041@gmail.com>
2021-10-23 17:45:30 +0530

    binarysearch

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
 
 namespace Algorithm
 {
@@ -6,11 +9,29 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1 . Permutation of String ");
-            Console.WriteLine("Enter the string : ");
-            string str = Console.ReadLine();
-            int length = str.Length;
-            Permutation.permute(str, 0, length - 1);
+            Console.WriteLine("1. Permutation of String ");
+            Console.WriteLine("2. Binary Search the Word ");
+            Console.Write("Enter your choice of operation : ");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter The  String : ");
+                    string str = Console.ReadLine();
+                    int length = str.Length;
+                    Permutation.permute(str, 0, length - 1);
+                    break;
+                case 2:
+                    string filepath = File.ReadAllText(@"E:\NILESH\C#\Algorithm\words.txt");
+                    List<string> list = new List<string>(filepath.Split(' '));
+                    list.Sort();
+                    BinarySearch.Search(list);
+                    break;
+                default:
+                    break;
+            }
+                       
+            
         }
     }
 }

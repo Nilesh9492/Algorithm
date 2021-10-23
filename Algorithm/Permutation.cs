6b24fa50470135e6637f0c4d2dcc644a4fe579a8
Nilesh9492 <nilesh94041@gmail.com>
2021-10-23 17:22:39 +0530

    permutation

---
+++ Algorithm/Permutation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Permutation
+    {
+        public static String swapping(String a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+        public static void permute(String str, int start, int end)
+        {
+            if (start == end)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    str = swapping(str, start, i);
+                    permute(str, start + 1, end);
+                    str = swapping(str, start, i);
+                }
+            }
+        }
+    }
+}
+
---
+++ Algorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 . Permutation of String ");
+            Console.WriteLine("Enter the string : ");
+            string str = Console.ReadLine();
+            int length = str.Length;
+            Permutation.permute(str, 0, length - 1);
+        }
+    }
+}
